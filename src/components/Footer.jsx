9e842968866c6692852d6f9bd16eec7fe1d0b048
Ayush Tripathi <103111856+ayushtripathi5359@users.noreply.github.com>
2025-04-05 20:30:20 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -142,13 +142,12 @@ const Footer = () => {
               variants={fadeIn('right', 1.0)}
               className="text-gray-600 text-sm"
             >
-              Copyright © {new Date().getFullYear()} codetutorbd.com
+              Copyright © {new Date().getFullYear()} 
             </motion.p>
             <motion.p 
               variants={fadeIn('left', 1.0)}
               className="text-gray-600 text-sm"
             >
-              Created by Md Al Mamun
             </motion.p>
           </motion.div>
         </motion.div>
